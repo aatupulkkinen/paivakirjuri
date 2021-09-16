d4b7d6ab9047981f198bae924712f1845d104dad
ruslanp <ruslan.potekhin@metropolia.fi>
2021-09-16 21:32:27 +0300

    Tietokantayhteyden testaus JUnit-testillä

---
+++ src/test/java/data/db/DBConnectionTest.java
@@ -0,0 +1,28 @@
+package data.db;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DBConnectionTest {
+
+    @Test
+    void testConnectionToDatabase() {
+        Connection connection = DatabaseConnection.getInstance().getConnection();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("select 1");
+            rs.first();
+            int testInt = rs.getInt(1);
+            assertEquals(testInt, 1);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
