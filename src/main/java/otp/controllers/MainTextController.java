d3c8e6e313972759bd9de0b4f33bdf924fef5510
sakuihalainen <sakuihalainen@github.com>
2021-10-06 14:09:30 +0300

    muutoksia MainTextControllerrin

--- src/main/java/otp/controllers/MainTextController.java
+++ src/main/java/otp/controllers/MainTextController.java
@@ -6,12 +6,25 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 import org.controlsfx.validation.Severity;
 import org.controlsfx.validation.ValidationSupport;
 import org.controlsfx.validation.Validator;
 import otp.Main;
 
 public class MainTextController {
+    @FXML
+    private TextFlow writing;
+    @FXML
+    private Button settings;
+    @FXML
+    private TextField searchField;
+    @FXML
+    private Button searchButton;
+    @FXML
+    private Button sidebar;
+    @FXML
+    private Button newMark;
 
 
 }
