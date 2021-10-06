c80871cd9dd7d2433c8aa6ccafa5715532e83661
ruslanp <ruslan.potekhin@metropolia.fi>
2021-10-06 21:44:43 +0300

    Tietokantamigraatio

--- src/main/java/otp/model/entities/User.java
+++ src/main/java/otp/model/entities/User.java
@@ -9,6 +9,17 @@ import java.util.Objects;
 public class User implements Serializable {
     private String name;
     private String password;
+    private String firstName;
+    private String secondName;
+
+    public User(String name, String password, String firstName, String secondName) {
+        this.name = name;
+        this.password = password;
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
+
+    public User() {}
 
     @Id
     @Column(name = "name", nullable = false, length = 40)
@@ -21,6 +32,26 @@ public class User implements Serializable {
     }
 
     @Basic
+    @Column(name = "name", nullable = false, length = 40)
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    @Basic
+    @Column(name = "first_name", nullable = false, length = 40)
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Basic
     @Column(name = "password", nullable = false, length = 128)
     public String getPassword() {
         return password;
@@ -30,17 +61,10 @@ public class User implements Serializable {
         this.password = password;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User that = (User) o;
-        return Objects.equals(name, that.name) && Objects.equals(password, that.password);
-    }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, password);
+        return Objects.hash(name, password, firstName, secondName);
     }
 
     @Override
@@ -49,5 +73,16 @@ public class User implements Serializable {
                 "name='" + name + '\'' +
                 ", password='" + password + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return name.equals(user.name)
+                && password.equals(user.password)
+                && firstName.equals(user.firstName)
+                && secondName.equals(user.secondName);
     }
 }
