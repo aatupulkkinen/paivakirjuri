10d25db41ed4ee8b71fa36dfb700224cb4a474b7
ruslanp <ruslan.potekhin@metropolia.fi>
2021-10-12 10:58:27 +0300

    valikko ja sivupalkki

--- src/main/java/otp/controllers/MainTextController.java
+++ src/main/java/otp/controllers/MainTextController.java
@@ -2,7 +2,12 @@ package otp.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.SplitPane;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import otp.Main;
 import otp.SceneController;
@@ -13,6 +18,11 @@ public class MainTextController {
     public Button searchButton;
     public Text dateText;
     public TextArea markContent;
+    public SplitPane splitPane;
+    public VBox innerContainer;
+    public ImageView backButton;
+    public MenuButton menu;
+    private boolean isSideViewOpen = true;
 
     public void settingsAction() {
         try {
@@ -28,12 +38,21 @@ public class MainTextController {
 
     }
 
-    public void openSideView(ActionEvent actionEvent) {
-
+    public void openSideView(MouseEvent actionEvent) {
+        if (isSideViewOpen) {
+            splitPane.setDividerPosition(0, 0.25);
+            innerContainer.setPrefWidth(900);
+            backButton.setRotate(180);
+        } else {
+            splitPane.setDividerPosition(0, 0.0);
+            innerContainer.setPrefWidth(1200);
+            backButton.setRotate(0);
+        }
+        isSideViewOpen = !isSideViewOpen;
     }
 
-    public void openMenu(ActionEvent actionEvent) {
-
+    public void openMenu(MouseEvent actionEvent) {
+        menu.fire();
     }
 }
 
